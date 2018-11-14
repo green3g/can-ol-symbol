b439351e45c7ea74a8ad4d445d80ddea4932ee57
Gregg Roemhildt <roemhildtg@gmail.com>
2018-11-13 18:07:17 -0600

    updates to demo/symbols

---
+++ index.js
@@ -0,0 +1,68 @@
+
+import Observable from 'ol/Observable';
+import Collection from 'ol/Collection';
+import Map from 'ol/Map';
+import Tile from 'ol/layer/Tile';
+import OSM from 'ol/source/OSM';
+import View from 'ol/View';
+import {fromLonLat} from 'ol/proj';
+import stache from 'can-stache';
+import Component from 'can-component';
+import 'ol/ol.css';
+import can from 'can-debug';
+window.can = can;
+
+import assignCanSymbols from './lib/assignCanSymbols';
+import assignCanListSymbols from './lib/assignCanListSymbols';
+
+assignCanSymbols(Observable.prototype);
+assignCanListSymbols(Collection.prototype);
+
+const map = new Map({
+  target: 'map',
+  layers: [
+    new Tile({
+      title: 'OSM',
+      source: new OSM(),
+    }),
+  ],
+  view: new View({
+    center: fromLonLat([37.41, 8.82]),
+    zoom: 4,
+  }),
+});
+
+Component.extend({
+  tag: 'lat-lon',
+  view: `
+  <p>{{map.view.center}}: {{map.view.resolution}}</p>
+  <ul>
+  {{#for(layer of map.layergroup.layers)}}<li>{{layer.title}}</li>{{/for}}
+  </ul>
+<p>{{map.layergroup.layers.length}}</p>
+  `,
+  ViewModel: {
+    map: {},
+  },
+});
+
+
+setTimeout(() => {
+  map.getLayers().push(new Tile({
+    source: new OSM(),
+  }));
+}, 10000);
+
+document.body.appendChild(stache('<lat-lon map:from="map" />')({
+  map,
+}));
+
+setTimeout(() => {
+  map.get('layergroup').get('layers').push(new Tile({
+    title: 'OSM',
+    source: new OSM(),
+  }));
+}, 1000);
+
+
+console.log(map);
