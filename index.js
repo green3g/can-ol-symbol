8a9df77ac08f126ea424f629e0fa0ae3c83c7c89
Gregg Roemhildt <roemhildtg@gmail.com>
2018-11-16 22:00:45 -0600

    clean up

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ import Collection from 'ol/Collection';
 import stache from 'can-stache';
 import can from 'can-debug';
 import canReflect from 'can-reflect';
+import Patcher from 'can-diff/patcher/patcher';
 
 import assignCanSymbols from './lib/assignCanSymbols';
 import assignCanListSymbols from './lib/assignCanListSymbols';
@@ -11,22 +12,18 @@ import assignCanListSymbols from './lib/assignCanListSymbols';
 assignCanSymbols(Observable.prototype);
 assignCanListSymbols(Collection.prototype);
 
-const array = new Collection(['hi', 'hey', 'hello'])
-;
-
-// canReflect.onPatches(array, function() {
-//   console.log(arguments);
-// });
-
+const array = new Collection(['hi', 'hey', 'hello']);
+const patcher = new Patcher(array);
+console.log(patcher);
 document.body.appendChild(stache(`
   {{#for(a of array)}}<li>{{a}}</li>{{/for}}
 `)({
   array,
-}))
-;
+}));
+
+console.log(array);
 
 array.push('hola');
-
 
 setTimeout(() => {
   array.removeAt(0);
