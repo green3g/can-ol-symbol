47d10e4a512d2ccd47bf1699733964cb9266aba3
Gregg Roemhildt <roemhildtg@gmail.com>
2018-11-16 23:19:21 -0600

    getting patches to work (sort of)

--- index.js
+++ index.js
@@ -2,9 +2,6 @@
 import Observable from 'ol/Observable';
 import Collection from 'ol/Collection';
 import stache from 'can-stache';
-import can from 'can-debug';
-import canReflect from 'can-reflect';
-import Patcher from 'can-diff/patcher/patcher';
 
 import assignCanSymbols from './lib/assignCanSymbols';
 import assignCanListSymbols from './lib/assignCanListSymbols';
@@ -13,8 +10,7 @@ assignCanSymbols(Observable.prototype);
 assignCanListSymbols(Collection.prototype);
 
 const array = new Collection(['hi', 'hey', 'hello']);
-const patcher = new Patcher(array);
-console.log(patcher);
+
 document.body.appendChild(stache(`
   {{#for(a of array)}}<li>{{a}}</li>{{/for}}
 `)({
@@ -26,9 +22,9 @@ console.log(array);
 array.push('hola');
 
 setTimeout(() => {
-  array.removeAt(0);
+  array.removeAt(2);
 }, 2000);
 
 setTimeout(() => {
   array.clear();
-}, 5000);
+}, 10000);
